284e18c04ec9f469e41382f641ed2b26a364807d
Alex <beatwad@gmail.com>
2022-11-23 13:17:53 +0300

    Remove chacking of previous notifications from the Telegram module, because we don't need it anymore.

--- sigbot/telegram_api/telegram_api.py
+++ sigbot/telegram_api/telegram_api.py
@@ -40,7 +40,6 @@ class TelegramBot(Thread):
         # bot parameters
         self.configs = configs[self.type]['params']
         self.chat_ids = self.configs['chat_ids']
-        self.prev_sig_limit = self.configs.get('prev_sig_minutes_limit', 25)
         self.max_notifications_in_row = self.configs.get('self.max_notifications_in_row', 3)
         self.updater = Updater(token=token, use_context=True)
         self.dispatcher = self.updater.dispatcher
@@ -102,21 +101,6 @@ class TelegramBot(Thread):
         self.images_to_delete.add(sig_img_path)
         return sig_img_path
 
-    def check_previous_notifications(self, sig_time: pd.Timestamp, sig_type: str, ticker: str,
-                                     timeframe: str, pattern: str) -> bool:
-        """ Check if previous notifications wasn't send short time before """
-        tmp = self.notification_df[
-                                    (self.notification_df['sig_type'] == sig_type) &
-                                    (self.notification_df['ticker'] == ticker) &
-                                    (self.notification_df['timeframe'] == timeframe) &
-                                    (self.notification_df['pattern'] == pattern)
-                                   ]
-        if tmp.shape[0] > 0:
-            latest_time = tmp['time'].max()
-            if sig_time - latest_time < pd.Timedelta(self.prev_sig_limit, "m"):
-                return False
-        return True
-
     def check_notifications(self):
         """ Check if we can send each notification separately or there are too many of them,
             so we have to send list of them in one message """
@@ -161,14 +145,13 @@ class TelegramBot(Thread):
             if [ticker, sig_type] not in message_list_tmp:
                 message_list_tmp.append([ticker, sig_type])
                 # add ticker info to notification list
-                if self.check_previous_notifications(sig_time, sig_type, ticker, timeframe, sig_pattern):
-                    send_flag = True
-                    if sig_pattern == 'HighVolume':
-                        text += f' • {ticker} \n '
-                    elif sig_type == 'buy':
-                        text += f' • {ticker}: Покупка \n'
-                    else:
-                        text += f' • {ticker}: Продажа \n'
+                send_flag = True
+                if sig_pattern == 'HighVolume':
+                    text += f' • {ticker} \n '
+                elif sig_type == 'buy':
+                    text += f' • {ticker}: Покупка \n'
+                else:
+                    text += f' • {ticker}: Продажа \n'
                 self.add_to_notification_history(sig_time, sig_type, ticker, timeframe, sig_pattern)
         if send_flag:
             self.send_message(chat_id, text)
@@ -198,32 +181,31 @@ class TelegramBot(Thread):
         # # get total and ticker statistics
         # result_statistics = message[8]
         # form message
-        if self.check_previous_notifications(sig_time, sig_type, ticker, timeframe, sig_pattern):
-            chat_id = self.chat_ids[sig_pattern]
-            # Form text message
-            text = f'{ticker} \n'
-            if sig_pattern == 'HighVolume':
-                pass
-            elif sig_type == 'buy':
-                text += 'Покупка \n'
-            else:
-                text += 'Продажа \n'
-            text += 'Продается на биржах: \n'
-            for exchange in sig_exchanges:
-                text += f' • {exchange}\n'
-            text += 'Ссылка на TradingView: \n'
-            clean_ticker = self.clean_ticker(ticker)
-            text += f"https://ru.tradingview.com/symbols/{clean_ticker}\n"
-            # text += f"https://ru.tradingview.com/chart/?symbol={sig_exchanges[0]}%3A{self.clean_ticker(ticker)}\n"
-            if clean_ticker[:-4] != 'BTC':
-                text += 'Ссылка на график с BTC: \n'
-                text += f"https://ru.tradingview.com/symbols/{clean_ticker[:-4]}BTC"
-                # text += f"https://ru.tradingview.com/chart/" \
-                #         f"?symbol={sig_exchanges[0]}%3A{self.clean_ticker(ticker)[:-4]}BTC"
-            # Send message + image
-            if sig_img_path:
-                self.send_photo(chat_id, sig_img_path, text)
-            time.sleep(1)
+        chat_id = self.chat_ids[sig_pattern]
+        # Form text message
+        text = f'{ticker} \n'
+        if sig_pattern == 'HighVolume':
+            pass
+        elif sig_type == 'buy':
+            text += 'Покупка \n'
+        else:
+            text += 'Продажа \n'
+        text += 'Продается на биржах: \n'
+        for exchange in sig_exchanges:
+            text += f' • {exchange}\n'
+        text += 'Ссылка на TradingView: \n'
+        clean_ticker = self.clean_ticker(ticker)
+        text += f"https://ru.tradingview.com/symbols/{clean_ticker}\n"
+        # text += f"https://ru.tradingview.com/chart/?symbol={sig_exchanges[0]}%3A{self.clean_ticker(ticker)}\n"
+        if clean_ticker[:-4] != 'BTC':
+            text += 'Ссылка на график с BTC: \n'
+            text += f"https://ru.tradingview.com/symbols/{clean_ticker[:-4]}BTC"
+            # text += f"https://ru.tradingview.com/chart/" \
+            #         f"?symbol={sig_exchanges[0]}%3A{self.clean_ticker(ticker)[:-4]}BTC"
+        # Send message + image
+        if sig_img_path:
+            self.send_photo(chat_id, sig_img_path, text)
+        time.sleep(0.5)
         self.add_to_notification_history(sig_time, sig_type, ticker, timeframe, sig_pattern)
         self.delete_images()
 

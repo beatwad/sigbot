02193015fa7e57ba387d0d5b7aad56d2c6e61c70
Alex <beatwad@gmail.com>
2022-12-07 11:19:27 +0300

    Change price statistics to E-ratio. Add tests for volume indicator.

--- sigbot/tests/test_indicators.py
+++ sigbot/tests/test_indicators.py
@@ -91,29 +91,20 @@ def test_price_change(df, timeframe, ticker, expected):
 
 @pytest.mark.parametrize('df, ticker, timeframe, expected',
                          [
-                          (df_btc_5m, 'BTC', '5m', [[0.00261, 0.169, 0.236, -0.00444, 0.00343,
-                                                    -0.5287, 0.55779, -0.66101, 0.6836],
-                                                    [3e-05, -0.0793, -0.0144, -0.00444, 0.00343,
-                                                     -0.5287, 0.55779, -0.66101, 0.6836],
-                                                    [-0.00096, -0.0799, 0.0639, -0.00444, 0.00343,
-                                                     -0.5287, 0.55779, -0.66101, 0.6836]]),
-                          (df_eth_5m, 'ETH', '5m', [[5e-05, 0.0191, -0.1887, -0.00629, 0.00707,
-                                                    -0.79934, 1.11272, -0.99277, 1.37686],
-                                                    [-0.00299, 0.2201, 0.2561, -0.00629, 0.00707,
-                                                     -0.79934, 1.11272, -0.99277, 1.37686],
-                                                    [-0.00093, -0.3034, -0.3124, -0.00629, 0.00707,
-                                                     -0.79934, 1.11272, -0.99277, 1.37686]]),
+                          (df_btc_5m, 'BTC', '5m', [[0.00084, 0.00328],
+                                                    [0.00415, 0.00328],
+                                                    [0.00053, 0.00328]]),
+                          (df_eth_5m, 'ETH', '5m', [[0.00082, 0.00438],
+                                                    [0.00358, 0.00438],
+                                                    [0.00095, 0.00438]]),
                           ], ids=repr)
 def test_high_volume(df, timeframe, ticker, expected):
     indicator = IndicatorFactory.factory('HighVolume', 'buy', configs)
     data_qty = 500
     df = indicator.get_indicator(df, ticker, timeframe, data_qty)
-    assert round(df.loc[223, 'close_price_change_lag_1'], 5) == expected[0][0]
-    assert round(df.loc[223, 'q_low_lag_1'], 5) == expected[0][3]
-    assert round(df.loc[223, 'q_high_lag_1'], 5) == expected[0][4]
-    assert round(df.loc[500, 'close_price_change_lag_1'], 5) == expected[1][0]
-    assert round(df.loc[500, 'q_low_lag_1'], 5) == expected[1][3]
-    assert round(df.loc[500, 'q_high_lag_1'], 5) == expected[1][4]
-    assert round(df.loc[998, 'close_price_change_lag_1'], 5) == expected[2][0]
-    assert round(df.loc[998, 'q_low_lag_1'], 5) == expected[2][3]
-    assert round(df.loc[998, 'q_high_lag_1'], 5) == expected[2][4]
+    assert round(df.loc[122, 'normalized_vol'], 5) == expected[0][0]
+    assert round(df.loc[122, 'quantile_vol'], 5) == expected[0][1]
+    assert round(df.loc[375, 'normalized_vol'], 5) == expected[1][0]
+    assert round(df.loc[375, 'quantile_vol'], 5) == expected[1][1]
+    assert round(df.loc[856, 'normalized_vol'], 5) == expected[2][0]
+    assert round(df.loc[856, 'quantile_vol'], 5) == expected[2][1]

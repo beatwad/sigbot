8a117c3646ea893652f1456e4da8fb1df052a600
beatwad <beatwad@gmail.com>
2023-10-23 10:50:57 +0300

    Gather load data and train function in one file

--- sigbot/bot/bot.py
+++ sigbot/bot/bot.py
@@ -82,13 +82,13 @@ class SigBot:
         else:
             self.exchanges = {
                             'ByBitPerpetual': {'API': GetData(**configs), 'tickers': [], 'all_tickers': []},
-                            'ByBit': {'API': GetData(**configs), 'tickers': [], 'all_tickers': []},
-                            'OKEXSwap': {'API': GetData(**configs), 'tickers': [], 'all_tickers': []},
-                            'OKEX': {'API': GetData(**configs), 'tickers': [], 'all_tickers': []},
-                            'BinanceFutures': {'API': GetData(**configs), 'tickers': [], 'all_tickers': []},
                             'Binance': {'API': GetData(**configs), 'tickers': [], 'all_tickers': []},
-                            'MEXCFutures': {'API': GetData(**configs), 'tickers': [], 'all_tickers': []},
-                            'MEXC': {'API': GetData(**configs), 'tickers': [], 'all_tickers': []}
+                            'OKEX': {'API': GetData(**configs), 'tickers': [], 'all_tickers': []},
+                            'ByBit': {'API': GetData(**configs), 'tickers': [], 'all_tickers': []},
+                            'MEXC': {'API': GetData(**configs), 'tickers': [], 'all_tickers': []},
+                            'BinanceFutures': {'API': GetData(**configs), 'tickers': [], 'all_tickers': []},
+                            'OKEXSwap': {'API': GetData(**configs), 'tickers': [], 'all_tickers': []},
+                            'MEXCFutures': {'API': GetData(**configs), 'tickers': [], 'all_tickers': []}
                             }
         self.max_prev_candle_limit = configs['Signal_params']['params']['max_prev_candle_limit']
         # Get API and ticker list for every exchange in list

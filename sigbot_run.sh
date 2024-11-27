a06400b64c24e9eaa771ec67f57c02dfc1356d7e
beatwad <beatwad@gmail.com>
2024-11-27 19:50:16 +0300

    Update .sh app start file

--- sigbot_run.sh
+++ sigbot_run.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 while true
 do
-  docker run -v ${PWD}/sigbot:/sigbot sigbot
-  sleep 60
-  docker rm $(docker ps -q --filter ancestor=sigbot --filter status=exited)
+  docker run --rm -v ${PWD}/sigbot:/sigbot sigbot
   sleep 60
 done
